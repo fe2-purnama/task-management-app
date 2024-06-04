b80908ca66cc3b014907b1a4f3bb4574ab366731
fhit2 <fauzihidayat2002@gmail.com>
2024-06-04 13:37:58 +0800

    Add Auth

---
+++ server/routes/register.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const registerController = require('../controller').register;
+const verifyUser = require('../library/verify');
+
+router.get('/', verifyUser.isLogout, registerController.formRegister);
+
+router.post('/save', verifyUser.isLogout, registerController.saveRegister);
+
+module.exports = router;
