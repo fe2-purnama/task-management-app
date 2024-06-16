8e91eac8e23d7460c5c4046ce5c841a1ae7ff4f6
fhit2 <fauzihidayat2002@gmail.com>
2024-06-16 14:51:39 +0800

    Update register dan login

--- backend/controller/loginController.js
+++ backend/controller/loginController.js
@@ -15,52 +15,52 @@ const loginAuth = function (req, res) {
     let username = req.body.username;
     let password = req.body.pass;
 
-    if (username && password) {
-        connection.query(
-            `SELECT * FROM user WHERE (email = ? OR username = ?) AND password = SHA2(?,512)`,
-            [username, username, password], function (error, results) {
-                if (error) throw error;
-                if (results.length > 0) {
-                    const payload = {
-                        userid: results[0].id_user,
-                        username: results[0].username,
-                        role: results[0].role
-                    };
-                    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
-                    req.session.token = token;
-                    req.session.loggedin = true;
-                    req.flash('color', 'success');
-                    req.flash('status', 'Success');
-                    req.flash('message', 'Login berhasil');
-
-                    // Send additional data in response
-                    res.status(200).json({
-                        username: results[0].username,
-                        email: results[0].email,
-                        role: results[0].role,
-                        token: token
-                    });
-
-                } else {
-                    req.flash('color', 'danger');
-                    req.flash('status', 'Oops..');
-                    req.flash('message', 'Akun tidak ditemukan');
-                    res.redirect('/login');
-                    return res.status(400).json({
-                        error: 'Invalid credentials'
-                    });
-                }
-            });
-    } else {
-        req.flash('color', 'warning');
-        req.flash('status', 'Warning');
-        req.flash('message', 'Please enter a valid email/username and password');
-        res.redirect('/login');
+    if (!username || !password) {
         return res.status(400).json({
-            error: 'Missing credentials'
+            error: 'Please enter a valid email/username and password'
         });
     }
+
+    connection.query(
+        `SELECT * FROM user WHERE (email = ? OR username = ?) AND password = SHA2(?,512)`,
+        [username, username, password], function (error, results) {
+            if (error) {
+                console.error('Database error:', error);
+                req.flash('color', 'danger');
+                req.flash('status', 'Oops..');
+                req.flash('message', 'Internal server error');
+                return res.status(500).redirect('/login');
+            }
+
+            if (results.length > 0) {
+                const payload = {
+                    userid: results[0].id_user,
+                    username: results[0].username,
+                    role: results[0].role
+                };
+                const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+                req.session.token = token;
+                req.session.loggedin = true;
+                req.flash('color', 'success');
+                req.flash('status', 'Success');
+                req.flash('message', 'Login berhasil');
+
+                return res.status(200).json({
+                    username: results[0].username,
+                    email: results[0].email,
+                    role: results[0].role,
+                    token: token
+                });
+
+            } else {
+                return res.status(400).json({
+                    error: 'Akun tidak ditemukan'
+                });
+            }
+        }
+    );
 }
+
 
 const logout = function (req, res) {
     req.session.destroy((err) => {
--- backend/controller/registerController.js
+++ backend/controller/registerController.js
@@ -19,70 +19,69 @@ const saveRegister = function (req, res) {
             'SELECT email, username FROM user WHERE email = ? OR username = ?',
             [email, username],
             function (error, results) {
-                if (error) throw error;
-                if (results.length > 0) {
-                    req.flash('color', 'danger');
-                    req.flash('status', 'Oops..');
-                    req.flash('message', 'Email atau username sudah terdaftar');
-                    res.redirect('/register');
-                } else {
-
-                    cloudinary.uploader.upload(defaultProfilePic, function(error, result) {
-                        if (error) {
-                            console.error('Cloudinary upload error:', error);
-                            req.flash('color', 'danger');
-                            req.flash('status', 'Oops..');
-                            req.flash('message', 'Terjadi kesalahan saat mengunggah foto profil');
-                            res.redirect('/register');
-                            return;
-                        }
-
-                        const profilePicUrl = result.secure_url;
-
-                        connection.query(
-                            'INSERT INTO user (username, email, password, role, foto, created_at) VALUES (?, ?, SHA2(?, 512), ?, ?, NOW())',
-                            [username, email, password, role, profilePicUrl],
-                            function (error, results) {
-                                if (error) throw error;
-                                const userId = results.insertId;
-                                const projects = ['Work', 'School', 'Home'];
-                                const projectQueries = projects.map(project => {
-                                    return new Promise((resolve, reject) => {
-                                        connection.query(
-                                            'INSERT INTO project (id_user, nama) VALUES (?, ?)',
-                                            [userId, project],
-                                            function (error, results) {
-                                                if (error) return reject(error);
-                                                resolve(results);
-                                            }
-                                        );
-                                    });
-                                });
-                                Promise.all(projectQueries)
-                                    .then(() => {
-                                        req.flash('color', 'success');
-                                        req.flash('status', 'Yes..');
-                                        req.flash('message', 'Registrasi berhasil');
-                                        res.redirect('/login');
-                                    })
-                                    .catch(error => {
-                                        console.error(error);
-                                        req.flash('color', 'danger');
-                                        req.flash('status', 'Oops..');
-                                        req.flash('message', 'Terjadi kesalahan saat menambahkan project');
-                                        res.redirect('/register');
-                                    });
-                            }
-                        );
-                    });
+                if (error) {
+                    console.error('Database query error:', error);
+                    res.status(500).json({ message: 'Terjadi kesalahan pada server' });
+                    return;
                 }
+                if (results.length > 0) {
+                    // Jika email atau username sudah terdaftar
+                    let existingField = results[0].email === email ? 'email' : 'username';
+                    res.status(409).json({ message: `${existingField} sudah terdaftar` });
+                    return;
+                }
+
+                cloudinary.uploader.upload(defaultProfilePic, function(error, result) {
+                    if (error) {
+                        console.error('Cloudinary upload error:', error);
+                        res.status(500).json({ message: 'Terjadi kesalahan saat mengunggah foto profil' });
+                        return;
+                    }
+
+                    const profilePicUrl = result.secure_url;
+
+                    connection.query(
+                        'INSERT INTO user (username, email, password, role, foto, created_at) VALUES (?, ?, SHA2(?, 512), ?, ?, NOW())',
+                        [username, email, password, role, profilePicUrl],
+                        function (error, results) {
+                            if (error) {
+                                console.error('Database query error:', error);
+                                res.status(500).json({ message: 'Terjadi kesalahan pada server' });
+                                return;
+                            }
+                            const userId = results.insertId;
+                            const projects = ['Work', 'School', 'Home'];
+                            const projectQueries = projects.map(project => {
+                                return new Promise((resolve, reject) => {
+                                    connection.query(
+                                        'INSERT INTO project (id_user, nama) VALUES (?, ?)',
+                                        [userId, project],
+                                        function (error, results) {
+                                            if (error) return reject(error);
+                                            resolve(results);
+                                        }
+                                    );
+                                });
+                            });
+                            Promise.all(projectQueries)
+                                .then(() => {
+                                    res.status(200).json({ message: 'Registrasi berhasil' });
+                                })
+                                .catch(error => {
+                                    console.error('Database query error:', error);
+                                    res.status(500).json({ message: 'Terjadi kesalahan saat menambahkan project' });
+                                });
+                        }
+                    );
+                });
             }
         );
     } else {
-        res.redirect('/register');
-        res.end();
+        res.status(400).json({ message: 'Data tidak lengkap' });
     }
 };
+
+
 
 module.exports = {
     formRegister,
